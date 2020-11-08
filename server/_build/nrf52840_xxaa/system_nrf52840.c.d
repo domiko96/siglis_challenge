12a720cce02671f75c1e5a40235af5ef1ff5fd05
Dominik Kogel <dominik.kogel@djk96.rocks>
2020-11-08 09:07:25 +0100

    initial commit

---
+++ server/_build/nrf52840_xxaa/system_nrf52840.c.d
@@ -0,0 +1,57 @@
+_build/nrf52840_xxaa/system_nrf52840.c.o: \
+ SDK/modules/nrfx/mdk/system_nrf52840.c \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stdint.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stdbool.h \
+ SDK/modules/nrfx/mdk/nrf.h SDK/modules/nrfx/mdk/nrf52840.h \
+ SDK/components/toolchain/cmsis/include/core_cm4.h \
+ SDK/components/toolchain/cmsis/include/cmsis_version.h \
+ SDK/components/toolchain/cmsis/include/cmsis_compiler.h \
+ SDK/components/toolchain/cmsis/include/cmsis_gcc.h \
+ SDK/components/toolchain/cmsis/include/mpu_armv7.h \
+ SDK/modules/nrfx/mdk/system_nrf52840.h \
+ SDK/modules/nrfx/mdk/nrf52840_bitfields.h \
+ SDK/modules/nrfx/mdk/nrf51_to_nrf52840.h \
+ SDK/modules/nrfx/mdk/nrf52_to_nrf52840.h \
+ SDK/modules/nrfx/mdk/compiler_abstraction.h \
+ SDK/modules/nrfx/mdk/nrf_erratas.h SDK/modules/nrfx/mdk/nrf51_erratas.h \
+ SDK/modules/nrfx/mdk/nrf52_erratas.h \
+ SDK/modules/nrfx/mdk/nrf53_erratas.h \
+ SDK/modules/nrfx/mdk/nrf91_erratas.h
+
+/usr/lib/gcc/arm-none-eabi/9.2.1/include/stdint.h:
+
+/usr/lib/gcc/arm-none-eabi/9.2.1/include/stdbool.h:
+
+SDK/modules/nrfx/mdk/nrf.h:
+
+SDK/modules/nrfx/mdk/nrf52840.h:
+
+SDK/components/toolchain/cmsis/include/core_cm4.h:
+
+SDK/components/toolchain/cmsis/include/cmsis_version.h:
+
+SDK/components/toolchain/cmsis/include/cmsis_compiler.h:
+
+SDK/components/toolchain/cmsis/include/cmsis_gcc.h:
+
+SDK/components/toolchain/cmsis/include/mpu_armv7.h:
+
+SDK/modules/nrfx/mdk/system_nrf52840.h:
+
+SDK/modules/nrfx/mdk/nrf52840_bitfields.h:
+
+SDK/modules/nrfx/mdk/nrf51_to_nrf52840.h:
+
+SDK/modules/nrfx/mdk/nrf52_to_nrf52840.h:
+
+SDK/modules/nrfx/mdk/compiler_abstraction.h:
+
+SDK/modules/nrfx/mdk/nrf_erratas.h:
+
+SDK/modules/nrfx/mdk/nrf51_erratas.h:
+
+SDK/modules/nrfx/mdk/nrf52_erratas.h:
+
+SDK/modules/nrfx/mdk/nrf53_erratas.h:
+
+SDK/modules/nrfx/mdk/nrf91_erratas.h:
